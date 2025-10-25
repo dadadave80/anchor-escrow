427ec773f12257730e12b9f0b9cfe35f9152683e
David Dada <daveproxy80@gmail.com>
2025-10-25 04:30:21 +0100

    Implement take accounts

--- programs/anchor-escrow-q4-25/src/instructions/take.rs
+++ programs/anchor-escrow-q4-25/src/instructions/take.rs
@@ -12,12 +12,61 @@ use anchor_spl::{
 
 use crate::Escrow;
 
-// #[derive(Accounts)]
-// pub struct Take<'info> {
-//      TODO: Implement Take Accounts
-// }
-
 // impl<'info> Take<'info> {
 //      TODO: Implement Take Instruction
 //      Includes Deposit, Withdraw and Close Vault
 // }
+#[derive(Accounts)]
+pub struct Take<'info> {
+    #[account(mut)]
+    pub taker: Signer<'info>,
+    #[account(mut)]
+    pub maker: SystemAccount<'info>,
+    pub mint_a: InterfaceAccount<'info, Mint>,
+    pub mint_b: InterfaceAccount<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = mint_a,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program,
+    )]
+    pub taker_ata_a: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_b,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program,
+    )]
+    pub taker_ata_b: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = mint_b,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program
+    )]
+    pub maker_ata_b: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        mut,
+        close = maker,
+        has_one = mint_a,
+        has_one = mint_b,
+        has_one = maker,
+        seeds = [b"escrow", maker.key().as_ref(), &escrow.seed.to_le_bytes()],
+        bump = escrow.bump
+    )]
+    pub escrow: Account<'info, Escrow>,
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority = escrow,
+        associated_token::token_program = token_program
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
